5f18712599969758cc10e5e74c45b71ccee202d5
crandays <casinad@gmail.com>
2025-03-09 11:42:43 +0100

    Delete utilities directory

--- utilities/numeric_utils.py
+++
@@ -1,7 +0,0 @@
-from decimal import Decimal
-
-def ensure_decimal(value):
-    """Convert value to Decimal if it's not already"""
-    if isinstance(value, Decimal):
-        return value
-    return Decimal(str(value))
